0c0aa9c83c0f8bbe1553905f39e7dbb1ffb7c803
LaurenS999 <74395888+LaurenS999@users.noreply.github.com>
2026-04-07 14:17:52 +0700

    feat: initialize express server and basic middleware

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+//Connect ke Frontend
+app.use(cors({
+  origin: "http://localhost:3000"
+}));
+
+//Middleware
+app.use(express.json());
+
+//Menjalankan server di Node.js
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server berjalan di port ${PORT}`);
+});
